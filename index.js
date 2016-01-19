59d719d20adfbe4cb4bb1a99493b36fc2b6a0a65
marcortw <github@schnueriger.net>
2016-01-19 22:34:44 +0100

    Major refactoring of the parsers and the index structure

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ var L4ep = require('./lib/model/Layer4Endpoint');
 var L7ep = require('./lib/model/Layer7Endpoint');
 var config = require('./lib/configloader');
 
-
-//var api = require('./lib/api.js');
-
 var Acdp = function () {
     var agentVersion = require('./package.json').version
     logger.info("Starting ACDP Submitter for Node.js version %s.", agentVersion);
@@ -27,174 +24,361 @@ var Acdp = function () {
     });
 };
 
-produce = function (endpoints, forConsumers) {
-    console.log('Produce in api!');
-};
-
-/**
- *
- * @param endpoints
- * @param fromProducers Array
- * @param description
- */
-consumeSpecific = function (endpoints, fromProducers, description) {
-    console.log('Consume in api! Endpoints:' + JSON.stringify(endpoints));
-
-    endpoints.forEach(function (entry) {
-        try {
-            var retObj = classifier.validateShorthand(entry);
-            logger.debug(JSON.stringify(retObj));
-        }
-        catch (err) {
-            logger.error(err.message);
-        }
-    });
-
-    var consumerObj = new Consumer();
-    consumerObj.addProducer()
-
-    var dObj = new Request();
-    dObj.request = {
-        'consumer': {
-            'description': description,
-            'consumes': [],
-            'fromProducers': []
-        }
-    };
-    //var objProducer = Request.defineProp(demand, 'producer', {
-    //    'description': description,
-    //    'produces': [],
-    //    'forConsumers': []
-    //});
-    //demand.consumer.consumes = [];
-    dObj.request.consumer.consumes.push({'endpoint': {'layer4Endpoint': {"applications": [endpoints]}}});
-    var result = dObj.validateAndSend(function (err, results) {
+produce = function (acdpShorthands, callback) {
+    starter(acdpShorthands, producerParser, function (err, result) {
         if (err) {
-            logger.error('Problem encountered at Request creation.');
+            if (callback) callback(err, null);
+        } else {
+            if (callback) callback(null, result);
         }
     });
-
-};
-
-consumePattern = function (pattern) {
-    //var demandA = new Request();
-    //Request.showCount();
-    //Request.defineProp(demandA,"a","b");
-    //demandA.defineProp(demandA,"a","b");
-
-    //var demandB = new Request();
-    //demandB.defineProp(demandB,"c","d");
-
-};
-
-consumeApplication = function (application) {
-
 };
 
 consume = function (acdpShorthands, callback) {
+    starter(acdpShorthands, consumerParser, function (err, result) {
+        if (err) {
+            if (callback) callback(err, null);
+        } else {
+            if (callback) callback(null, result);
+        }
+    });
+};
+
+starter = function (acdpShorthands, parser, callback) {
     var reqObj = new Request();
 
     if (acdpShorthands.constructor === Array) {
-        acdpShorthands.forEach(function (acdpShorthand) {
-            parse(acdpShorthand, function (err, consumerOrProducer) {
+        async.each(acdpShorthands, function (acdpShorthand, callback) {
+            parser(acdpShorthand, function (err, consumerOrProducer) {
                 if (err) {
-                    logger.error(err);
-                    if (callback) callback(err)
+                    callback(err)
                 } else {
                     reqObj.addDemand(consumerOrProducer);
+                    callback();
                 }
             });
+        }, function (err) {
+            if (err) {
+                logger.error(err);
+                if (callback) callback(err)
+            } else {
+                reqObj.validateAndSend(function (err, result) {
+                    if (err) {
+                        if (callback) callback(err, null);
+                    } else {
+                        if (callback) callback(null, result);
+                    }
+                })
+            }
         });
     } else {
-        console.log('single obj');
-        parse(acdpShorthands, function (err, consumerOrProducer) {
+        parser(acdpShorthands, function (err, consumerOrProducer) {
             if (err) {
                 logger.error(err);
                 if (callback) callback(err)
             } else {
                 reqObj.addDemand(consumerOrProducer);
+                reqObj.validateAndSend(function (err, result) {
+                    if (err) {
+                        if (callback) callback(err, null);
+                    } else {
+                        if (callback) callback(null, result);
+                    }
+                })
             }
         });
     }
+};
 
-    function parse(shorthand, callback) {
-        logger.debug(Object.keys(shorthand).length);
-        if (Object.keys(shorthand).length > 2) {
-            throw new Error('Invalid number of key-value pairs');
-        }
+var producerParser = (function (shorthand, callback) {
+    if (Object.keys(shorthand).length > 2) {
+        throw new Error('Invalid number of key-value pairs');
+    }
 
-        // We have only one key value pair
-        if (Object.keys(shorthand).length == 1) {
-            for (var key in shorthand) {
-                var value = shorthand[key];
-                switch (key.toUpperCase()) {
-                    case "URL":
-                        classifier.objectsFromUrl(value, function (err, results) {
-                            if (err) {
-                                callback(err, null);
-                            } else {
-                                var l3Obj = results[0];
-                                var l4Obj = results[1];
-                                var consObj = new Consumer();
-                                consObj.addProducer(l3Obj);
-                                consObj.addProduct(l4Obj);
-                                callback(null, consObj);
-                            }
-                        });
-                        break;
-
-
-                    case "APP":
-                        var l4Obj = new L4ep();
-                        l4Obj.addSpecial(constants.SPECIAL_ANY);
-
-                        var l7Obj = new L7ep();
-                        l7Obj.addApplication(value)
-
-                        var consObj = new Consumer();
-                        consObj.addProduct(l4Obj);
-                        consObj.addProducer(l7Obj);
+    // if we have only one key value pair...
+    if (Object.keys(shorthand).length == 1) {
+        resolveShorthand(shorthand, function (err, result) {
+            if (err) {
+                callback(err);
+            } else {
+                logger.debug(result);
+                switch (result.type) {
+                    case "PORT":
+                        var consObj = new Producer();
+                        var l3ep = new L3ep();
+                        l3ep.addSpecial({"type": constants.SPECIAL_UNKNOWN});
+                        consObj.addConsumer(l3ep);
+                        consObj.addProduct(result.l4ep);
                         callback(null, consObj);
                         break;
-
-
+                    case "APP":
+                        result.l4ep.addSpecial(constants.SPECIAL_UNKNOWN); // do this because the caller only specified an App, no port
+                        var consObj = new Consumer();
+                        consObj.addProduct(result.l4ep);
+                        consObj.addProducer(result.l7ep);
+                        callback(null, consObj);
+                        break;
                     case "IP":
+                        result.l4ep.addSpecial(constants.SPECIAL_UNKNOWN); // do this because the caller only specified an IP-Address
+                        var consObj = new Consumer();
+                        consObj.addProducer(result.l3ep);
+                        consObj.addProduct(result.l4ep);
+                        callback(null, consObj);
                         break;
-
-                    case "FOR":
-                    case "FROM":
-                        if (callback) callback(new Error('The "FOR" and "FROM" keys can only be used together with another key.'), null);
-                        break;
-
                     default:
                         var errMsg = 'Invalid key supplied: ' + key;
                         logger.error(errMsg);
                         if (callback) callback(new Error(errMsg), null);
                 }
             }
-        }
-
-        // We have exactly two key value pairs
-        if (Object.keys(shorthand).length == 2) {
-
-        }
+        });
     }
 
-    reqObj.validateAndSend(function (err, result) {
-        if (err) {
-            logger.error(err)
-            if (callback) callback(err, null);
-        } else {
-            if (callback) callback(null, result);
+    // We have exactly two key value pairs
+    if (Object.keys(shorthand).length == 2) {
+        var objects = {};
+        async.forEachOf(shorthand, function (value, key, callback) {
+            var tmpObj = {};
+            tmpObj[key] = value;
+            resolveShorthand(tmpObj, function (err, result) {
+                if (err) {
+                    return callback(err);
+                } else {
+                    objects[result.type] = result;
+                    callback();
+                }
+            })
+        }, function (err) {
+            if (err) {
+                callback(err)
+            } else {
+                var validObject = false;
+                if (objects.FOR && objects.PORT) {
+                    var prodObj = new Producer();
+                    prodObj.addProduct(objects.PORT.l4ep);
+                    if (objects.FOR.nested.l3ep) prodObj.addConsumer(objects.FOR.nested.l3ep);
+                    if (objects.FOR.nested.l7ep) prodObj.addConsumer(objects.FOR.nested.l7ep);
+                    validObject = true;
+                    callback(null, prodObj);
+                }
+
+                if (!validObject) callback(new Error("No allowed shorthand notation"));
+            }
+
+        });
+    }
+});
+
+var consumerParser = (function (shorthand, callback) {
+    if (Object.keys(shorthand).length > 2) {
+        throw new Error('Invalid number of key-value pairs');
+    }
+
+    // We have only one key value pair
+    if (Object.keys(shorthand).length == 1) {
+        resolveShorthand(shorthand, function (err, result) {
+            if (err) {
+                callback(err);
+            } else {
+                logger.debug(result);
+                switch (result.type) {
+                    case "URL":
+                        var consObj = new Consumer();
+                        consObj.addProducer(result.l3ep);
+                        consObj.addProduct(result.l4ep);
+                        callback(null, consObj);
+                        break;
+                    case "APP":
+                        var consObj = new Consumer();
+                        var l4ep = new L4ep();
+                        l4ep.addSpecial(constants.SPECIAL_UNKNOWN); // do this because the caller only specified an App, no port
+                        consObj.addProduct(l4ep);
+                        consObj.addProducer(result.l7ep);
+                        callback(null, consObj);
+                        break;
+                    case "IP":
+                        var consObj = new Consumer();
+                        var l4ep = new L4ep();
+                        l4ep.addSpecial(constants.SPECIAL_UNKNOWN); // do this because the caller only specified an IP-Address
+
+                        consObj.addProducer(result.l3ep);
+                        consObj.addProduct(l4ep);
+                        callback(null, consObj);
+                }
+            }
+        });
+    }
+
+    // We have exactly two key value pairs
+    if (Object.keys(shorthand).length == 2) {
+        var objects = {};
+        async.forEachOf(shorthand, function (value, key, callback) {
+            var tmpObj = {};
+            tmpObj[key] = value;
+            resolveShorthand(tmpObj, function (err, result) {
+                if (err) {
+                    return callback(err);
+                } else {
+                    objects[result.type] = result;
+                    callback();
+                }
+            })
+        }, function (err) {
+            if (err) {
+                callback(err)
+            } else {
+                if (objects.IP && objects.PORT) {
+                    var consObj = new Consumer();
+                    consObj.addProduct(objects.PORT.l4ep);
+                    consObj.addProducer(objects.IP.l3ep);
+                    callback(null, consObj);
+                }
+
+                if (objects.APP && objects.PORT) {
+                    var consObj = new Consumer();
+                    consObj.addProduct(objects.PORT.l4ep);
+                    consObj.addProducer(objects.APP.l7ep);
+                    callback(null, consObj);
+                }
+
+                if (objects.APP && objects.SPECIAL) {
+                    var consObj = new Consumer();
+                    var l4ep = new L4ep();
+                    l4ep.addSpecial(objects.SPECIAL.special);
+                    consObj.addProduct(l4ep);
+                    consObj.addProducer(objects.APP.l7ep);
+                    callback(null, consObj);
+                }
+
+                callback(new Error("No allowed shorthand notation"));
+            }
+
+        });
+    }
+});
+
+function resolveShorthand(result, callback) {
+    for (var key in result) {
+        var value = result[key];
+        switch (key.toUpperCase()) {
+            case "URL":
+                classifier.objectsFromUrl(value, function (err, results) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        var l3Obj = results[0];
+                        var l4Obj = results[1];
+                        var retObj = {
+                            type: "URL",
+                            l3ep: l3Obj,
+                            l4ep: l4Obj
+                        };
+                        callback(null, retObj);
+                    }
+                });
+                break;
+
+
+            case "APP":
+                var l7Obj = new L7ep();
+                l7Obj.addApplication(value);
+
+                var retObj = {
+                    type: "APP",
+                    l7ep: l7Obj
+                };
+
+                callback(null, retObj);
+                break;
+
+            case "TCP":
+                var l4Obj = new L4ep();
+                l4Obj.addPort(constants.TCP, value);
+                var retObj = {
+                    type: "PORT",
+                    l4ep: l4Obj
+                };
+                callback(null, retObj);
+                break;
+
+            case "UDP":
+                var l4Obj = new L4ep();
+                l4Obj.addPort(constants.UDP, value);
+                var retObj = {
+                    type: "PORT",
+                    l4ep: l4Obj
+                };
+                callback(null, retObj);
+                break;
+
+
+            case "IP":
+                var l3Obj = new L3ep();
+                l3Obj.addIpAddr(value);
+
+                var retObj = {
+                    type: "IP",
+                    l3ep: l3Obj
+                };
+
+                callback(null, retObj);
+                break;
+
+            case "SPECIAL":
+                var specialObj = {"type": value};
+
+                var retObj = {
+                    type: "SPECIAL",
+                    special: specialObj
+                };
+
+                callback(null, retObj);
+                break;
+
+            case "FOR":
+            case "FROM":
+                if (typeof value === 'object' && value !== null) {
+                    async.waterfall([
+                        function (callback) {
+                            if (typeof value === 'object' && value !== null) {
+                                resolveShorthand(value, function (err, result) {
+                                    if (err) {
+                                        callback(err)
+                                    } else {
+                                        console.log(result);
+                                        callback(null, result);
+                                    }
+                                });
+                            }
+                        },
+                        function (nested, callback) {
+                            var retObj = {};
+                            retObj.type = key.toUpperCase();
+                            retObj.nested = nested;
+                            callback(null, retObj);
+                        }
+                    ], function (err, result) {
+                        if (err) {
+                            callback(err)
+                        }
+                        callback(null, result);
+                    });
+                } else {
+                    callback(new Error("FOR and FROM keywords can only be used together with an object as a value."));
+                }
+
+                break;
+
+            default:
+                var errMsg = 'Invalid key supplied: ' + key;
+                logger.error(errMsg);
+                if (callback) callback(new Error(errMsg), null);
         }
-    })
-};
+    }
+}
 
 module.exports = {
     Acdp: new Acdp,
-    consumeSpecific: consumeSpecific,
-    consumePattern: consumePattern,
-    consumeApplication: consumeApplication,
     produce: produce,
     consume: consume
 };
